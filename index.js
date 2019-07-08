cefb1cef80d15cb56ddb983a2749a81b7e9d8a8b
Jony Oinas <35299547+oinasjo@users.noreply.github.com>
2019-07-08 21:21:58 +0300

    init method ready

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Logger = require('./logfileWriter.js');
 
-Logger.DEBUGLOG("This is debugLog test (DEBUG MODE)");
-Logger.ERRORLOG("This was an erroLog test (DEBUG MODE)");
-Logger.INFOLOG("This was infoLog test (DEBUG MODE)");
+Logger.INIT("./logs", "test");
+
+Logger.DEBUG("INDEX debug");
+Logger.ERROR("INDEX debug");
+Logger.INFO("INDEX debug");
--- logfileWriter.js
+++ logfileWriter.js
@@ -5,11 +5,20 @@
 
 // Imports
 const fs = require('fs');
+const os = require('os');
 
 const LogLevel = {
     DEBUG: 0,
     INFO: 1,
     ERROR: 2
+}
+
+const OS = {
+    WIN: 0,
+    LINUX: 1,
+    DARWIN: 2,
+    FREEBSD: 3,
+    SUNOS: 4 
 }
 
 /**
@@ -20,45 +29,82 @@ let Logger = {
 
     _logFileLocation: "",
     _logFileName: "",
-    _defaultURL: "./logs/logFileWriter.log",
+    _defaultLocation: "./logs/",
+    _defaultName: "default.log",
     _debugMode: false,
+    _default: true,
 
     /**
-     * Initializes the logger. If init is not called, the Logger uses ./logs/
-     * { String } _logFileLocation, { String } _logFileName
+     * Initializes the logger. If init is not called, the Logger uses project's ./logs/ folder to store a .log file named 'default.log'
+     * @param {String} logFileLocation
+     * @param {String} logFileName
      */
-    INIT: function (_logFileLocation, _logFileName) {
+    INIT: function (logFileLocation, logFileName) {
         try {
-            Utils.validateString(_logFileLocation, _logFileName);
+            Utils.validateString(logFileLocation, logFileName);
         } catch (err) {
             throw err;
         }
-        this._logFileLocation = _logFileLocation;
-        this._logFileName = _logFileName;
+
+        if (logFileLocation.slice(-1) !== "/") {
+            this._logFileLocation = logFileLocation + "/";
+        } else {
+            this._logFileLocation = logFileLocation;
+        }
+
+        if (logFileName.includes(".log") || logFileName.includes(".txt") || logFileName.includes(".xml")) {
+            this._logFileName = logFileName;
+        } else {
+            this._logFileName = logFileName + ".log";
+        }
+        this._setDefault(false);
     },
-    DEBUGLOG: function (str) {
+    DEBUG: function (str) {
         this._writeLog(
             str,
             { logLevel: LogLevel.DEBUG }
         );
     },
-    INFOLOG: function (str) {
+    INFO: function (str) {
         this._writeLog(
             str,
             { logLevel: LogLevel.INFO }
         );
     },
-    ERRORLOG: function (str) {
+    ERROR: function (str) {
         this._writeLog(
             str,
             { logLevel: LogLevel.ERROR }
         );
     },
+    _checkPackageJSON: function (operatingSystem) {
+        if (operatingSystem === OS.WIN) {
+            console.log("win32");
+        } else {
+            console.log("not windows");
+        }
+    },
     _runInDebugMode: function (flag) {
         this._debugMode = flag;
     },
+    _setDefault: function (bool) {
+        this._default = bool;
+    },
+    _useDefaultLocation: function () {
+        return this._default;
+    },
     _writeLog: function (debugString, config) {
-        let stream = fs.createWriteStream(this._defaultURL, { flags: 'a' });
+        let stream;
+        let logURL;
+        let defaultURL;
+        if (this._useDefaultLocation()) {
+            defaultURL = this._defaultLocation + this._defaultName;
+            stream = fs.createWriteStream(defaultURL, { flags: 'a' });
+        } else {
+            logURL = this._logFileLocation + this._logFileName;
+            stream = fs.createWriteStream(logURL, { flags: 'a' });
+        }
+
         let logLevel = config.logLevel;
 
         if (this._debugMode) {
@@ -66,7 +112,6 @@ let Logger = {
                 console.log("Log file has been updated");
             });
         }
-
         stream.write(this._buildString(debugString, logLevel));
         stream.end();
     },
@@ -75,16 +120,38 @@ let Logger = {
         let msg = "";
         switch (logLevel) {
             case LogLevel.DEBUG:
-                msg = "[" + timeStamp + "] DEBUG: " + str + "\n"; 
+                msg = "[" + this._getTimeStamp() + "] DEBUG: " + str + "\n"; 
                 break;
             case LogLevel.INFO:
-                msg = "[" + timeStamp + "] INFO: " + str + "\n"; 
+                msg = "[" + this._getTimeStamp() + "] INFO: " + str + "\n"; 
                 break;
             case LogLevel.ERROR:
-                msg = "[" + timeStamp + "] ERROR: " + str + "\n"; 
+                msg = "[" + this._getTimeStamp() + "] ERROR: " + str + "\n"; 
                 break;
         }
         return msg;
+    },
+    _getTimeStamp: function () {
+        let date = new Date();
+
+        let hour = date.getHours();
+        hour = (hour < 10 ? "0" : "") + hour;
+
+        let min = date.getMinutes();
+        min = (min < 10 ? "0" : "") + min;
+
+        let sec = date.getSeconds();
+        sec = (sec < 10 ? "0" : "") + sec;
+
+        let year = date.getFullYear();
+
+        let month = date.getMonth() + 1;
+        month = (month < 10 ? "0" : "") + month;
+
+        let day = date.getDate();
+        day = (day < 10 ? "0" : "") + day;
+
+        return year + "-" + month + "-" + day + " " + hour + ":" + min + ":" + sec;
     }
 };
 
